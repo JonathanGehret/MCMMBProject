4aeb377f84111f30578aeb29a3172cda3a0ca98c
JonathanGehret <jonathan@gehrets.de>
2021-06-09 22:42:28 +0200

    erste versuche mit gbif

--- MCMMB_1.R
+++ MCMMB_1.R
@@ -1,7 +1,9 @@
 #package librarys
 
+# install.packages("XML") # zum einlesen von .xml files
 library(rgdal)
 library(raster)
+#library(XML)
 
 
 #Daten einlesen
@@ -44,3 +46,19 @@ head(regio)
 plot(regio)
 plot(first, add=T, col='blue')
 plot(second, add=T, col = "red")
+
+
+# how to use gbif with R: https://www.youtube.com/watch?v=RACfDLBIL8A
+
+# comparison gbif dataset for Antigone rubicunda (eine Reiher art)  
+# https://www.gbif.org/species/9295466
+# GBIF.org (09 June 2021) GBIF Occurrence Download https://doi.org/10.15468/dl.6m2srw
+
+
+
+#gbif_antigone <- xmlParse(file= "data/gbif/antigone_rubicunda/dataset/4fa7b334-ce0d-4e88-aaae-2e0c138d049e.xml")
+#str(gbif_antigone)
+#plot(gbif_antigone)
+
+
+
