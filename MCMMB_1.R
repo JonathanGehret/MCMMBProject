2ac16d35403563013644ac8c25b35b6e0ea92450
paulleister <paul.leister@hotmail.de>
2021-06-09 18:04:07 +0200

    first try

--- MCMMB_1.R
+++ MCMMB_1.R
@@ -1,1 +1,36 @@
+#package librarys
+
+library(rgdal)
+library(raster)
+
+
 #Daten einlesen
+
+setwd("/data")
+
+grid <- readOGR("Birdlife_Papua.shp", integer64="allow.loss")
+
+#grid@proj4string <- CRS("+proj=longlat +ellps=WGS84 +no_defs") # remember to include coordinate system
+
+str(grid)
+head(grid)
+
+first <- grid[1,]
+
+head(first)
+str(first)
+
+plot(first)
+
+
+regio <- readOGR("Papua_region.shp", integer64="allow.loss")
+
+str(regio)
+head(regio)
+
+
+
+str(first)
+
+plot(regio)
+plot(first, add=T, col='blue')
