fc3b08d2c9b67ea3e40a5b59a0c1991f624b58b2
JonathanGehret <jonathan@gehrets.de>
2021-06-09 22:06:07 +0200

    Kommentare hinzugefügt und Liste aller Species erstellt

--- MCMMB_1.R
+++ MCMMB_1.R
@@ -8,6 +8,10 @@ library(raster)
 
 setwd("/data")
 
+# ich würde vorschlagen, den working space zum Ort der Projekt-datei zu setzen und dann
+# immer, wenn data benötigt wird, das entsprechend anzugeben, zb:
+# grid <- readOGR("data/Birdlife_Papua.shp", integer64="allow.loss")
+ 
 grid <- readOGR("Birdlife_Papua.shp", integer64="allow.loss")
 
 #grid@proj4string <- CRS("+proj=longlat +ellps=WGS84 +no_defs") # remember to include coordinate system
@@ -15,23 +19,27 @@ grid <- readOGR("Birdlife_Papua.shp", integer64="allow.loss")
 str(grid)
 head(grid)
 
+# Liste aller species:
+
+species <- grid$SCINAME 
+species <- data.frame(species)
+
+# getting individual species
+
 first <- grid[1,]
 second <- grid[2,]
 
 head(first)
 str(first)
 
-plot(first)
-
+# reading in island borders
 
 regio <- readOGR("Papua_region.shp", integer64="allow.loss")
 
 str(regio)
 head(regio)
 
-
-
-str(first)
+# plotting
 
 plot(regio)
 plot(first, add=T, col='blue')
