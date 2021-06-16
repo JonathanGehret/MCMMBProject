7ff4444943b5bae376c282e1a7cfede489bd86e5
JonathanGehret <jonathan@gehrets.de>
2021-06-16 17:10:12 +0200

    ADDED CHIRS bribs

--- MCMMB_GBIF.R
+++ MCMMB_GBIF.R
@@ -201,11 +201,17 @@ plot(regio)
 
 # ind_birbs
 ind_birbs = read.csv("data/gbif/aves_indonesia/0303155-200613084148143.csv",header = TRUE, sep = "\t")
-ind_birbs = ind_birbs[!(ind_birbs$decimalLatitude=="" | ind_birbs$decimalLongitude==""),]
+chris_birbs = read.csv("data/gbif/birds_indonesia.csv",header = TRUE, sep = "\t")
+ind_birbs = chris_birbs
+ind_birbs_corrected = ind_birbs[!(is.na(ind_birbs$decimalLatitude) | ind_birbs$decimalLatitude=="" | is.na(ind_birbs$decimalLongitude) | ind_birbs$decimalLongitude==""),]
+
+# create points
+ind_birbs_points = st_as_sf(ind_birbs_corrected, coords = c("decimalLongitude","decimalLatitude"), crs = 4326)
 
 #crop to indenesian papua
  ###
 
 # plot
 plot(regio)
-plot(st_geometry(new_birb_points), pch=16, col="navy", add = TRUE)
+plot(st_geometry(ind_birbs_points), pch=16, col="navy", add = TRUE)
+plot(st_geometry(ind_birbs_points), pch=16, col="green", add = TRUE)
