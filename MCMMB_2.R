21a352770f8748c3aeaacc97c98b91b92baf942c
paulleister <paul.leister@hotmail.de>
2021-06-17 13:24:42 +0200

    ÖLÄ

--- MCMMB_2.R
+++ MCMMB_2.R
@@ -7,8 +7,9 @@ library(raster)
 
 #Daten einlesen
 
+# birdlife --> shows the distriution of all spotted birds in papua
 
-birdlife <- readOGR("data/Papua_Birdlife_project/Birdlife_Papua.shp", integer64="allow.loss")
+#birdlife <- readOGR("data/Papua_Birdlife_project/Birdlife_Papua.shp", integer64="allow.loss")
 
 
 # nature reserve data
@@ -21,44 +22,39 @@ papua_borders <- readOGR("data/Indicator/nature_reserve_papua/papua_borders/IDN_
 
 
 #elevation map
-elev <- raster("data/Indicator/elevationmn30_grd")
+#elev <- raster("data/Indicator/elevation/mn30_grd")
+#par(mfrow=c(1,1))
+#plot(elev)
 
+
+#regio_elev <- intersect(elev, regio)
+#plot(regio_elev)
+#writeRaster(regio_elev, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/elevation/regio_elev"), format="GTiff", overwrite=TRUE)
+
+regio_elev <- raster("data/Indicator/elevation/regio_elev.tif")
+plot(regio_elev)
 
 #Temperatur
-bio1 <- raster("data/CHELSA_bio10_01.tif") # temperature*10
+#temp <- raster("data/Indicator/CHELSA_bio10_01.tif") # temperature*10
+#regio_temp <- intersect(temp, regio)
+#plot(regio_temp)
+
+#writeRaster(regio_temp, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/Temperatur/regio_temp"), format="GTiff", overwrite=TRUE)
+regio_temp <- raster("data/Indicator/Temperatur/regio_temp.tif")
+plot(regio_temp)
+
 
 #Precipitation
-bio12 <- raster("data/CHELSA_bio10_12.tif") # precipitation
+
+#precip <- raster("data/Indicator/CHELSA_bio10_12.tif") # precipitation
+#regio_precip <- intersect(precip, regio)
+#plot(regio_precip)
+
+#writeRaster(regio_precip, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/Precipitation/regio_precip"), format="GTiff", overwrite=TRUE)
+regio_precip <- raster("data/Indicator/Precipitation/regio_precip.tif")
+plot(regio_precip)
 
 
-str(grid)
-head(grid)
-
-# Liste aller species:
-
-species <- grid$SCINAME 
-species <- data.frame(species)
-
-# getting individual species
-
-first <- grid[1,]
-second <- grid[2,]
-antigone <- grid[42,]
-
-head(first)
-str(first)
-
-# reading in island borders
-
-regio <- readOGR("data/Papua_Birdlife_project/Papua_region.shp", integer64="allow.loss")
-
-str(regio)
-head(regio)
-
-regio$ADM1_NAME
-plot(grid)
-plot(regio[1,], add = T, col = "green")
-plot(regio[2,], add = T, col = "blue")
 
 
 #intersect land_reserv
@@ -70,16 +66,63 @@ plot(regio[2,], add = T, col = "blue")
 #writeOGR(land_reserv, dsn ="C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/nature_reserve_papua", layer= "land_reserv", driver = "ESRI Shapefile")
 
 #land-reserv
-land_reserv <- readOGR("data/Indicator/nature_reserve_papua/land_reserv.shp", integer64="allow.loss")
+land_reserv <- readOGR("data/Indicator/nature_reserve_papua/land_reserv/land_reserv.shp", integer64="allow.loss")
+
+
+# reading in island borders
+
+regio <- readOGR("data/Papua_Birdlife_project/Papua_region.shp", integer64="allow.loss")
+
+
+#Population-density
+#popul <- raster("data/Indicator/population/idn_msk_pop.grd") 
+#plot(popul)
+
+
+#regio_popul <- intersect(popul, regio)
+#plot(regio_popul)
+
+#writeRaster(regio_popul, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/population/regio_popul"), format="GTiff", overwrite=TRUE)
+regio_popul <- raster("data/Indicator/population/regio_popul.tif")
+plot(regio_popul)
+
+
+
+
+
+#Primary-forest
+primforest <- raster("data/Indicator/Primary_Forest/Asia_2001_primary.tif") 
+plot(primforest)
+
+
+regio_primforest <- intersect(primforest, regio)
+plot(regio_primforest)
+
+writeRaster(regio_primforest, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/Primary_Forest/regio_primforest"), format="GTiff", overwrite=TRUE)
+regio_primforest <- raster("data/Indicator/Primary_Forest/regio_primforest.tif")
+plot(regio_primforest)
+
+
 
 
 # plotting
 
 plot(regio)
-plot(first, add=T, col='blue')
-plot(second, add=T, col = "red")
-plot(antigone, add=T, col = "green")
-plot(nature_reserve, add=T, col= "purple")
-plot(papua_borders, add=T, col= "orange")
+plot(papua_borders, add=T, col = "red")
+plot(land_reserv, add=T, col='blue')
 
 
+plot(regio_elev)
+plot(regio, add=T)
+
+plot(regio_precip)
+plot(regio, add=T)
+
+plot(regio_temp)
+plot(regio, add=T)
+
+plot(regio_popul)
+plot(regio, add=T)
+
+plot(regio_primforest)
+plot(regio, add=T)
