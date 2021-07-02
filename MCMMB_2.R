4df7b45409c582051ce08c7493ccf77d4d36ce26
paulleister <paul.leister@hotmail.de>
2021-07-02 15:50:44 +0200

    öjkl

--- MCMMB_2.R
+++ MCMMB_2.R
@@ -6,11 +6,6 @@ library(raster)
 library(rgeos)
 
 
-Tif-daten zuschneiden:
--population
--natur-reserve
--primary-forest
-
 
 #Daten einlesen
 
@@ -21,12 +16,13 @@ Tif-daten zuschneiden:
 
 # nature reserve data
 
-nature_reserve <- readOGR("data/Indicator/nature_reserve_papua/nature_reserve/nature_reserve.shp", integer64="allow.loss")
+nature_reserve_project <- readOGR("data/Indicator/nature_reserve_papua/nature_reserve/nature_reserve.shp", integer64="allow.loss")
+nature_reserve_project@proj4string = CRS("+proj=longlat +ellps=WGS84 +no_defs")
 
 #Papua borders
 
-papua_borders <- readOGR("data/Indicator/nature_reserve_papua/papua_borders/IDN_adm1.shp", integer64="allow.loss")
-
+papua_borders_project <- readOGR("data/Indicator/nature_reserve_papua/papua_borders/IDN_adm1.shp", integer64="allow.loss")
+papua_borders_project@proj4string = CRS("+proj=longlat +ellps=WGS84 +no_defs")
 
 #elevation map
 #elev <- raster("data/Indicator/elevation/mn30_grd")
@@ -77,13 +73,13 @@ plot(regio_precip)
 #writeOGR(land_reserv, dsn ="C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/nature_reserve_papua", layer= "land_reserv", driver = "ESRI Shapefile")
 
 #land-reserv
-land_reserv <- readOGR("data/Indicator/nature_reserve_papua/land_reserv/land_reserv.shp", integer64="allow.loss")
-plot(land_reserv)
+land_reserv_project <- readOGR("data/Indicator/nature_reserve_papua/land_reserv/land_reserv.shp", integer64="allow.loss")
 
+land_reserv_project@proj4string = CRS("+proj=longlat +ellps=WGS84 +no_defs")
 # reading in island borders
 
-regio <- readOGR("data/Papua_Birdlife_project/Papua_region.shp", integer64="allow.loss")
-
+regio_project <- readOGR("data/Papua_Birdlife_project/Papua_region.shp", integer64="allow.loss")
+regio_project@proj4string = CRS("+proj=longlat +ellps=WGS84 +no_defs")
 
 #Population-density
 #popul <- raster("data/Indicator/population/idn_msk_pop.grd") 
@@ -94,29 +90,42 @@ regio <- readOGR("data/Papua_Birdlife_project/Papua_region.shp", integer64="allo
 #plot(regio_popul)
 
 #writeRaster(regio_popul, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/population/regio_popul"), format="GTiff", overwrite=TRUE)
-regio_popul <- raster("data/Indicator/population/regio_popul.tif")
+regio_popul <- raster("data/Indicator/population/idn_msk_pop.grd")
 plot(regio_popul)
 
+
+
+regio_population <- crop(regio_popul, regio_precip)
+
+population_resampled <- projectRaster(regio_population,regio_precip,method = 'bilinear')
+population_resampled
+
+writeRaster(population_resampled, filename=file.path("data/Indicator/population/population_resampled"), format="GTiff", overwrite=TRUE)
+
+population_resampled <- raster("data/Indicator/population/population_resampled.tif")
+plot(population_resampled)
 
 
 
 
 #Primary-forest
-#primforest <- raster("data/Indicator/Primary_Forest/Asia_2001_primary.tif") 
-#plot(primforest)
+primforest <- raster("data/Indicator/Primary_Forest/Asia_2001_primary.tif")
+plot(primforest)
 
+regio_primforest <- crop(primforest, regio_precip)
 
-#regio_primforest <- intersect(primforest, regio)
-#plot(regio_primforest)
+primforest_resampled <- resample(regio_primforest,regio_precip,method = 'bilinear')
+primforest_resampled
+plot(primforest_resampled)
 
-#writeRaster(regio_primforest, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/Primary_Forest/regio_primforest"), format="GTiff", overwrite=TRUE)
-regio_primforest <- raster("data/Indicator/Primary_Forest/regio_primforest.tif")
-plot(regio_primforest)
+writeRaster(primforest_resampled, filename=file.path("C:/Users/Paul/Desktop/3_Semester/modern concepts and methods in macroecology and biogeography/Projekt/MCMMBProject/data/Indicator/Primary_Forest/primforest_resampled"), format="GTiff", overwrite=TRUE)
+primforest_resampled <- raster("data/Indicator/Primary_Forest/primforest_resampled.tif")
+plot(primforest_resampled)
 
 
 #Landcover
 
-landcover <- raster("data/Indicator/landcover/IDN_msk_cov.grd")  #ob das passt
+landcover <- raster("data/Indicator/landcover/IDN_msk_cov.grd") 
 plot(landcover)   
 
 regio_precip <- raster("data/Indicator/Precipitation/regio_precip.tif")
@@ -153,3 +162,17 @@ plot(regio, add=T)
 
 plot(regio_primforest)
 plot(regio, add=T)
+
+
+
+
+#Verschneiden der shp-files mit der grid-shp-file 
+land_reserv_project
+nature_reserve_project
+papua_borders_project
+
+
+
+
+
+
