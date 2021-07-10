54c679568672a56e2d7dc650dfa745cd0574f13f
paulleister <paul.leister@hotmail.de>
2021-07-10 18:27:52 +0200

    gk

--- plotting_sdms.R
+++ plotting_sdms.R
@@ -4,9 +4,28 @@
 # to-do: loop over layers
 
 # compare gbif with elevation
-plot(elevation)
-plot(biomod_proj@proj@val@layers[[1]], add = T, col =  rgb(red = 0.5, green = 0.5, blue = 0.5, alpha = 0.5))
 
-# cmopare gbif with iucn
-plot(biomod_proj@proj@val@layers[[1]])
-plot(iucn_birds$`Pachycephala lorentzi`, add = T, col =  rgb(red = 0, green = 0, blue = 1, alpha = 0.5))
+par(mfrow=c(2,3))
+
+bio = biomod_proj@proj@val@layers
+
+for (i in bio) {
+plot(elevation)
+plot(i, add = T, col =  rgb(red = 0.5, green = 0.5, blue = 0.5, alpha = 0.5))
+
+}
+
+
+#compare IUCN with GBIF
+par(mfrow=c(2,3))
+
+bio = biomod_proj@proj@val@layers
+
+for (i in bio) {
+  plot(i)
+  plot(iucn_birds$`Pachycephala lorentzi`, add = T, col =  rgb(red = 0, green = 0, blue = 1, alpha = 0.5))
+  
+}
+
+
+
