b14dcbc4a667d8cc0ef2f935b4c6b2f16a0df8e5
JonathanGehret <jonathan@gehrets.de>
2021-08-18 12:51:35 +0200

    advances

---
+++ unused_scripts/MCMMB_libraries.R
@@ -0,0 +1,9 @@
+#package libraries
+
+library(biomod2) # for calculation of sdms
+library(rgdal)
+library(raster)
+library(rgeos)
+library(sf)
+library(spatialEco) # for intersecting with point.in.poly
+library(dplyr) # for removing species with n < threshold 
